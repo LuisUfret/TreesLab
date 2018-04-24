aa092b705eb60a6a903a7c4e16bf1a77b33d60f1
luisufret <luisufret@10.31.47.31>
2018-04-23 21:45:27 -0600

    third

--- TreeLab2_Student/src/treeClasses/LinkedBinaryTree2.java
+++ TreeLab2_Student/src/treeClasses/LinkedBinaryTree2.java
@@ -14,15 +14,15 @@ public class LinkedBinaryTree2<E extends Comparable<E>> extends LinkedBinaryTree
 	private void recInsert(Position<E> r, E e) {
 		int c = e.compareTo(r.getElement()); 
 		if (c < 0) 
-			if (!hasLeft(r))
-				addLeft(r, e); 
-			else 
-				recInsert(left(r), e); 
-		else 
 			if (!hasRight(r))
 				addRight(r, e); 
 			else 
-				recInsert(right(r), e); 		
+				recInsert(right(r), e); 
+		else 
+			if (!hasLeft(r))
+				addLeft(r, e); 
+			else 
+				recInsert(left(r), e); 		
 	}
 
 }
